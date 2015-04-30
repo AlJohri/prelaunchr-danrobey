85311e0b8fc967694020156c70daf6674a2cd9a8
Deepak Kundu <dpk@brahmadpk.com>
2015-04-30 08:22:02 +0530

    Ready to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,14 @@
 lock '3.3.5'
 
 set :application, 'prelaunchr_default'
+
 set :repo_url, 'https://github.com/brahmadpk/prelaunchr-default'
+
 set :deploy_to, '/home/brahmadpk/webapps/prelaunch_default'
+
 set :default_env, { path: "/home/brahmadpk/webapps/prelaunch_default/bin:$PATH", gem_home: "/home/brahmadpk/webapps/prelaunch_default/gems" }
+
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 after "deploy:started", "figaro:setup"
 after "deploy:updating", "figaro:symlink"
