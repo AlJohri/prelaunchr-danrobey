dd7c5c707a4e9842cb29be827b9b3893a1cb7dc0
Deepak Kundu <dpk@brahmadpk.com>
2015-04-30 07:14:51 +0530

    Ready to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.3.5'
 
 set :application, 'prelaunchr_default'
 set :repo_url, 'https://github.com/brahmadpk/prelaunchr-default'
-set :deploy_to, '/home/brahmadpk/webapps/prelaunchr_default'
+set :deploy_to, '/home/brahmadpk/webapps/prelaunch_default'
 set :default_env, { path: "/home/brahmadpk/webapps/prelaunch_default/bin:$PATH", gem_home: "/home/brahmadpk/webapps/prelaunch_default/gems" }
 
 after "deploy:started", "figaro:setup"
