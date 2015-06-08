45e73aa53432ab98dffafd2bbc5e401a2efeb1e2
Deepak Kundu <dpk@brahmadpk.com>
2015-06-08 20:02:30 +0530

    Removed favicon, added text to refer page, customized email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+        @twitter_message = "Can't wait for @thenosacrificediet to launch.  I am going to be #losingweightwithoutsacrfice for free."
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
