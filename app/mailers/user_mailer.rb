f812353f844f684dab610176b216edfee666e018
Al Johri <al.johri@gmail.com>
2015-07-29 12:50:47 -0400

    remove 'harrys' from mailers and views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-    default from: "Harry's <welcome@harrys.com>"
+    default from: "The No Sacrifice Diet <welcome@thenosacrificediet.com>"
 
     def signup_email(user)
         @user = user
-        @twitter_message = "Can't wait for @thenosacrificediet to launch.  I am going to be #losingweightwithoutsacrfice for free."
+        @twitter_message = "Can't wait for @nosacrificediet to launch. I am going to be #losingweightwithoutsacrfice for free."
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
