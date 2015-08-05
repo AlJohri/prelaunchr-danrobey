98a69049869982ca380be9605e1c4522ed5935ee
Al Johri <al.johri@gmail.com>
2015-08-05 14:57:49 -0400

    add referrals_count to admin dashboard (for real this time)

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register User do
 
+  index do
+    column :id
+    column :email
+    column :referral_code
+    column :referrer_id
+    column :referrals_count
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+
   csv do
     column :id
     column :email
